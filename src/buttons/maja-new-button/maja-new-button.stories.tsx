859e80cb7c44b5ae9b073f32b0559c07201c9db4
joseph.lenton@askattest.com <joseph.lenton@askattest.com>
2021-11-10 02:03:06 +0100

    feat: add story for new-button

---
+++ src/buttons/maja-new-button/maja-new-button.stories.tsx
@@ -0,0 +1,15 @@
+import '@/core/css'
+import React from 'react'
+import { ComponentMeta } from '@storybook/react'
+import { NewButton as NewButtonComponent } from '.'
+
+export default {
+  title: 'Buttons/NewButton',
+  component: NewButtonComponent,
+  argTypes: {},
+} as ComponentMeta<typeof NewButtonComponent>
+
+export const NewButton = () => {
+  return <NewButtonComponent />
+}
+NewButton.storyName = "NewButton"
