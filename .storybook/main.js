3aeeacc8bae7a2aaaa062d239f7951d825287faf
joseph.lenton@askattest.com <joseph.lenton@askattest.com>
2021-11-20 15:41:06 +0100

    chore: upgrade dependencies

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,14 +5,23 @@ module.exports = {
   styles: [
     "../src/core/css/*.css",
   ],
+
   stories: [
     "../src/**/*.stories.mdx",
     "../src/**/*.stories.@(js|jsx|ts|tsx)"
   ],
+
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-postcss"
+    {
+      name: '@storybook/addon-postcss',
+      options: {
+        postcssLoaderOptions: {
+          implementation: require('postcss'),
+        },
+      },
+    },
   ],
 
   webpackFinal: async config => {
