fbc66fb2b030cbfdd7ee5b1f6e02abb15bad4fa1
joseph.lenton@askattest.com <joseph.lenton@askattest.com>
2021-11-20 15:03:08 +0100

    feat: add editor card horizontal layout

--- src/editor/core/components/maja-editor-card/index.tsx
+++ src/editor/core/components/maja-editor-card/index.tsx
@@ -1,14 +1,26 @@
 import React from 'react'
-import { concatClassNames } from 'maja-util'
+import classnames from 'classnames'
 
 import './style.css'
 
 export interface EditorCardProps {
   className ?: string
+
+  direction ?: EditorCardDirection
 }
 
+export type EditorCardDirection =
+  | 'vertical'
+  | 'horizontal'
+
 export const EditorCard : React.FunctionComponent<EditorCardProps> = props => {
-  return <div className={concatClassNames('maja-editor-card', props.className)}>
+  const className = classnames({
+    'maja-editor-card': true,
+    [`maja-editor-card--direction-${props.direction}`]: !! props.direction,
+    [props.className || '']: !! props.className,
+  })
+
+  return <div className={className}>
     {props.children}
   </div>
 }
