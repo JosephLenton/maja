c02c41ea521119e20ecb2a8cf76a0ab9f830b9fd
joseph.lenton@askattest.com <joseph.lenton@askattest.com>
2021-11-22 22:44:44 +0100

    feat: sidebar heading border now only shows when scrolling

--- src/sidebars/components/maja-sidebar/index.tsx
+++ src/sidebars/components/maja-sidebar/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { concatClassNames } from '@maja-ui/util'
 import { SidebarHeading } from '../../internal-components/maja-sidebar-heading'
 
@@ -11,11 +11,37 @@ export interface SidebarProps {
 
 export const Sidebar : React.FunctionComponent<SidebarProps> = props => {
   const className = `maja-sidebar ${props.title ? 'maja-sidebar--top-padding' : ''}`
+  const contentRef = useRef<HTMLDivElement|null>(null)
+  const [hasBottomBorder, setHasBottomBorder] = useState(false)
+
+  useEffect(() => {
+    const onScroll = () => {
+      const dom = contentRef.current
+      if (!dom) {
+        return
+      }
+
+      setHasBottomBorder(dom.scrollTop > 0)
+    }
+
+    // Call once to initialise
+    onScroll()
+
+    const dom = contentRef.current
+    if (!dom) {
+      return
+    }
+
+    dom.addEventListener('scroll', onScroll)
+    return () => {
+      dom.removeEventListener('scroll', onScroll)
+    }
+  }, [contentRef])
 
   return <div className={concatClassNames(className, props.className)}>
-    {props.title && <SidebarHeading>{props.title}</SidebarHeading>}
+    {props.title && <SidebarHeading bottomBorder={hasBottomBorder}>{props.title}</SidebarHeading>}
 
-    <div className="maja-sidebar__content">
+    <div ref={contentRef} className="maja-sidebar__content">
       {props.children}
     </div>
   </div>
---
+++ src/sidebars/internal-components/maja-sidebar-heading/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { H2 } from '@/headings'
+import { concatClassNames } from '@maja-ui/util'
+import classnames from 'classnames'
+
+import './style.css'
+
+export interface SidebarHeadingProps {
+  className?: string
+  bottomBorder: boolean
+}
+
+export const SidebarHeading : React.FunctionComponent<SidebarHeadingProps> = props => {
+  const className = classnames({
+    'maja-sidebar-heading': true,
+    'maja-sidebar-heading--bottom-border': props.bottomBorder,
+    [props.className || '']: !!props.className,
+  })
+
+  return <H2 className={className}>
+    {props.children}
+  </H2>
+}
