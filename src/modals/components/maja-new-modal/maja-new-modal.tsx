68ff2111a7aa68976d31e102596f2a578c1437f6
josephlenton@gmail.com <josephlenton@gmail.com>
2022-02-27 23:04:55 +0000

    feat: add text fields to NewModal

--- src/modals/components/maja-new-modal/maja-new-modal.tsx
+++ src/modals/components/maja-new-modal/maja-new-modal.tsx
@@ -2,6 +2,9 @@ import React from 'react'
 import { MajaNewNameModalBase } from '../../bases/maja-new-name-modal-base'
 
 export interface NewModalProps {
+  title?: string
+  fieldLabel?: string
+  confirmText?: string
   onCancel: () => void
   onConfirm: (newName: string) => void
 }
@@ -9,9 +12,9 @@ export interface NewModalProps {
 export const NewModal : React.VoidFunctionComponent<NewModalProps> = props => {
   return <MajaNewNameModalBase
     name=""
-    title="New"
-    displayName="Name"
-    confirmText="Create"
+    title={props.title || "New"}
+    displayName={props.fieldLabel || "Name"}
+    confirmText={props.confirmText || "Create"}
     onCancel={props.onCancel}
     onConfirm={props.onConfirm}
   />
